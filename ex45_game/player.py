bec5e6c50292f7f27fe9de430dbd8142e7bd32ea
Ben Ross <benjross@cs.washington.edu>
2013-06-21 08:26:58 -0700

    updated player, might refactor later

--- ex45_game/player.py
+++ ex45_game/player.py
@@ -4,9 +4,14 @@ class Player(object):
     def __init__(self, attack):
         self.attack = attack
 
+    attacks = {
+              'punch': self.attack.punch(0, 0),
+              'kick': self.attack.kick(0, 0)
+              }
     def attack(self, move):
-        pass
+        Player.attacks.get(move)
 
 class Bob(Player):
-    def attack(self, move):
-        pass
+    attacks['punch'] = self.attack.punch(15, .25)
+    attacks['kick'] = self.attack.special(25, .185)
+    attacks['cool guy punch'] = self.attack.special(45, .1)
