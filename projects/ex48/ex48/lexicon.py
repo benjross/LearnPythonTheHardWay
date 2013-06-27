2ac2099fd6a43d1fe577d53e27d035d40135b4cd
Ben Ross <benjross@cs.washington.edu>
2013-06-27 08:30:31 -0700

    updated ex48 lexicon

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -1,15 +1,30 @@
-direction = ['north', 'south', 'east', 'west', 'down', 'up', 'left', 'right',
-        'back']
+directions = {'north', 'south', 'east', 'west', 'down', 'up', 'left', 'right',
+        'back'}
 
-verbs = ['go', 'stop', 'kill', 'eat']
+verbs = {'go', 'stop', 'kill', 'eat'}
 
-stops = ['the', 'in', 'of', 'from', 'at', 'it']
+stops = {'the', 'in', 'of', 'from', 'at', 'it'}
 
-nouns = ['door', 'bear', 'pincess', 'cabinet']
+nouns = {'door', 'bear', 'princess', 'cabinet'}
 
 def scan(phrase):
     words = phrase.split()
     ret_words = []
     for word in words:
-        ret_words.append(('direction', word))
+        temp = word.lower()
+        if temp in directions:
+            ret_words.append(('direction', word))
+        elif temp in verbs:
+            ret_words.append(('verb', word))
+        elif temp in stops:
+            ret_words.append(('stop', word))
+        elif temp in nouns:
+            ret_words.append(('noun', word))
+        else:
+            try:
+                num = int(word)
+                ret_words.append(('number', num))
+            except ValueError:
+                ret_words.append(('error', word))
+
     return ret_words
