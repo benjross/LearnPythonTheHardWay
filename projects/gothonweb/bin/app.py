c023baf34b2cc6addb3f440db27dfac7c0155633
Ben Ross <benjross@cs.washington.edu>
2013-06-28 21:38:11 -0700

    finished ex50+

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,15 +1,21 @@
 import web
 
 urls = (
-        '/', 'index'
+        '/(.*)', 'index'
         )
 
 app = web.application(urls, globals())
 
+render = web.template.render('templates/')
+
 class index:
-    def GET(self):
-        greeting = "Hello World"
-        return greeting
+    def GET(self, message):
+        #greeting = "Hello World"
+        #i = web.input(greeting=None)
+        #return render.index(i.greeting)
+        return render.index(message)
+        #return render.index(greeting = greeting)
+        #return render.foo()
 
 if __name__ == "__main__":
     app.run()
