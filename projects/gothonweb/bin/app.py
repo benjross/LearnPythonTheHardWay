5417bfbb9e06c8b14601d65042afa00cd7107125
Ben Ross <benjross@cs.washington.edu>
2013-11-03 16:14:47 +0100

    made some updates to ex51

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -14,19 +14,24 @@ class index:
 
     def POST(self):
         form = web.input(greet="Hello", name="Nobody", datafile={})
+        # check for empty strings
+        if not form.greet:
+            form.greet = "Hello"
+        if not form.name:
+            form.name = "Nobody"
         greeting = "%s, %s" % (form.greet, form.name)
         filename = form.datafile.filename
         if filename:
-            # replaces the windows-style slashes with linux ones.
-            filepath = form.datafile.filename.replace('\\','/')
+            # replace spaces
+            filename = filename.replace(' ', '_')
+            # escape hashes (otherwise it gets interpretted as python comment)
+            filename = filename.replace('#', 'hash')
             # splits the and chooses the last part (the filename with extension)
-            filename = filepath.split('/')[-1]
+            filename = filename.split('/')[-1]
             # creates the file where the uploaded file should be stored
-            fout = open('static/'+ filename,'w')
-            # writes the uploaded file to the newly created file.
-            fout.write(form.datafile.file.read())
-            # closes the file, upload complete.
-            fout.close()
+            with open('static/'+ filename,'w') as fout:
+                # writes the uploaded file to the newly created file.
+                fout.write(form.datafile.file.read())
         return render.index(greeting, filename)
 
 if __name__ == "__main__":
